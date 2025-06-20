a8fb3b266da6ef362d5b099ae12d93376339bc7b
YourGitHubUsername <nikhilrajput3006@gmail.com>
2025-06-20 18:28:55 +0530

    add

--- palindrome.java
+++ palindrome.java
@@ -1,38 +1,41 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class palindrome {
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in);
-//   int n=sc.nextInt();
-//   int a=n;
-//   int b=0;
-//   while(a>0){
-//     int d=n%10;
-//     b=b*10+d;
-//     a=a/10;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int a=n;
+  int b=0;
+  while(a>0){
+    int d=a%10;
+    b=b*10+d;
+    a=a/10;
   
-//   }
-//       if(b==n){
-//         System.out.println("palindrome");
-//       }else{
-//         System.out.println("not palindrome");
-//       }
-
-//     }
-// }
-import java.util.*;
-public class palindrome{
-  public static void main(String[]args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int temp=n;
-    int d=0;
-    while(temp!=0){
-      temp=temp/10;
-      d++;
-      
-    }
-   
- System.out.println(d);
   }
+      if(b==n){
+        System.out.println("palindrome");
+      }else{
+        System.out.println("not palindrome");
+      }
+
+    }
 }
+// import java.util.*;
+// public class palindrome{
+//   public static void main(String[]args){
+//     Scanner sc=new Scanner(System.in);
+//     int n=sc.nextInt();
+//     int t=n;
+//     int d=0;
+//     while(t>0){
+//       int b=t%10;
+//       d=d*10+b;
+//       t=t/10;
+
+    
+      
+//     }
+   
+//  System.out.println(n);
+//   }
+// }
