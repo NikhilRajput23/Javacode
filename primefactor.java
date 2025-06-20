664c2e9b901736d34d42d71b480f8d4b44ea8d65
YourGitHubUsername <nikhilrajput3006@gmail.com>
2025-06-20 20:26:25 +0530

    ads primenumber

--- primefactor.java
+++ primefactor.java
@@ -1,16 +1,38 @@
+// import java.util.Scanner;
+
+// public class primefactor {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int div=2;div<=n;div++){
+// while(n%div==0){
+//               n=n/div;
+//             System.out.print(div);
+//         }
+//     }
+      
+//         }
+//         }
+
 import java.util.Scanner;
 
-public class primefactor {
-    public static void main(String[] args) {
+public class primefactor{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
         int n=sc.nextInt();
-        for(int div=2;div<=n;div++){
-while(n%div==0){
-              n=n/div;
-            System.out.print(div);
+        int count=0;
+        for(int div=2;div*div<=n;div++){
+           if(n%div==0){
+            count++;
+           }
+        }
+           if(count==0){
+            System.out.println("prime");
+           }else{
+            System.out.println("Not prime");
+           }
         }
     }
-      
-        }
-        }
-
+}
