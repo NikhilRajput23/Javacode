7ed547fe3908c24ef4a55db50bbe0a8ebc7d34e4
YourGitHubUsername <nikhilrajput3006@gmail.com>
2025-06-21 16:56:04 +0530

    new add

--- prime.java
+++ prime.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
 
-public class prime {
-    public static void main(String [] args){
+// public class prime {
+//     public static void main(String [] args){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         for(int i=0;i<a;i++){
+//             int n=sc.nextInt();
+//                         int count=0;
+
+//             for(int j=2;j*j<=n;j++){
+//                 if(n%j==0){
+//                     count++;
+//              break;
+//                 }
+                
+//                 }
+//                 if(count==0){
+//                     System.out.println("Prime");
+//                 }else{
+//                     System.out.println("Not Prime");
+
+//             }
+//         }
+        
+//     }
+// }
+public class prime{
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        for(int i=0;i<a;i++){
+        for(int i=0;i<=a;i++){
             int n=sc.nextInt();
-                        int count=0;
-
+            int c=0;
             for(int j=2;j*j<=n;j++){
                 if(n%j==0){
-                    count++;
-             break;
-                }
-                
-                }
-                if(count==0){
-                    System.out.println("Prime");
-                }else{
-                    System.out.println("Not Prime");
 
+                
+                c++;
+                }
+            }
+            if(c==0){
+                System.out.println("prime");
+            }else{
+                System.out.println("Not Prime");
             }
         }
-        
     }
 }
