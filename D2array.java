2815c05322f18ebdeffcfd0da60f17326b717b67
YourGitHubUsername <nikhilrajput3006@gmail.com>
2025-08-18 21:10:24 +0530

    add

--- D2array.java
+++ D2array.java
@@ -6,14 +6,14 @@ public class D2array{
     int col=sc.nextInt();
     int [] []arr=new int[row][col];
     for(int i=0;i<row;i++){
-         for(int j=0;i<col;j++){
+         for(int j=0;j<col;j++){
          arr[i][j]=sc.nextInt();
 
     }
 }
- for(int i=0;i<row;i++){
-         for(int j=0;i<col;j++){
-        System.out.print(arr[i][j]+" ");
+for(int i=0;i<row;i++){
+         for(int j=0;j<col;j++){
+        System.out.print(arr[i][j] + " ");
 
     }
     System.out.println();
--- primefactor.java
+++ primefactor.java
@@ -14,26 +14,26 @@
 //         }
 //         }
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class primefactor{
-//     public static void main(String[] args){
-//         Scanner sc=new Scanner(System.in);
-//         int t=sc.nextInt();
-//         for(int i=0;i<t;i++){
-//         int n=sc.nextInt();
-//         int count=0;
-//         for(int div=2;div*div<=n;div++){
-//            if(n%div==0){
-//             count++;
-//            }
-//         }
-//            if(count==0){
-//             System.out.println("prime");
-//            }else{
-//             System.out.println("Not prime");
-//            }
-//         }
-//     }
-// }
+public class primefactor{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+        int n=sc.nextInt();
+        int count=0;
+        for(int j=2;j*j<=n;j++){
+           if(n%j==0){
+            count++;
+           }
+        }
+           if(count==0){
+            System.out.println("prime");
+           }else{
+            System.out.println("Not prime");
+           }
+        }
+    }
+}
 
